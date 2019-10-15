3f5fd4859c8ea74bbbb290e7131be486744c7d4f
catbeans <workemailzoop@gmail.com>
2019-10-14 22:03:46 -0400

    in progress

--- Percolation.java
+++ Percolation.java
@@ -1,13 +1,14 @@
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 /* *****************************************************************************
  *  Name:
  *  Date:
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    private boolean grid[][];
-    private WeightedQuickUnionUF QU;
+    private boolean[][] grid;
+    private WeightedQuickUnionUF qU;
     private int inputN;
 
     // creates n-by-n grid, with all sites initially blocked
@@ -16,17 +17,17 @@ public class Percolation {
             throw new IllegalArgumentException(
                     "This grid size is not valid. Please enter a value greater than 0.");
         }
-        QU = new WeightedQuickUnionUF(n * n + 2);
+        qU = new WeightedQuickUnionUF(n * n + 2);
         inputN = n;
         grid = new boolean[n][n]; // rows, cols
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 grid[i][j] = false;
                 if (i == 0) {
-                    QU.union(n * i + j, n * n); // join all of top row with "top" virtual element
+                    qU.union(n * i + j, n * n); // join all of top row with "top" virtual element
                 }
                 if (i == n - 1) {
-                    QU.union(n * i + j, n * n + 1); // join bottom row with "bottom" virtual element
+                    qU.union(n * i + j, n * n + 1); // join bottom row with "bottom" virtual element
                 }
                 // System.out.printf(" %b", grid[i][j]);
             }
@@ -46,22 +47,22 @@ public class Percolation {
         }
         if (row - 1 >= 0) {
             if (isOpen(row - 1, col)) {
-                QU.union(inputN * (row) + col, inputN * (row - 1) + col);
+                qU.union(inputN * (row) + col, inputN * (row - 1) + col);
             }
         }
         if (row + 1 < inputN) {
             if (isOpen(row + 1, col)) {
-                QU.union(inputN * (row) + col, inputN * (row + 1) + col);
+                qU.union(inputN * (row) + col, inputN * (row + 1) + col);
             }
         }
         if (col - 1 >= 0) {
             if (isOpen(row, col - 1)) {
-                QU.union(inputN * (row) + col, inputN * (row) + col - 1);
+                qU.union(inputN * (row) + col, inputN * (row) + col - 1);
             }
         }
         if (col + 1 < inputN) {
             if (isOpen(row, col + 1)) {
-                QU.union(inputN * (row) + col, inputN * (row) + col + 1);
+                qU.union(inputN * (row) + col, inputN * (row) + col + 1);
             }
         }
         // make sure checking row is within bounds
@@ -86,7 +87,7 @@ public class Percolation {
         if (row < 0 || row >= inputN || col < 0 || col >= inputN) {
             throw new IllegalArgumentException("Arguments are out of bounds");
         }
-        return QU.connected(inputN * (row) + col, inputN * inputN);
+        return qU.connected(inputN * (row) + col, inputN * inputN);
     }
 
     // returns the number of open sites
@@ -104,7 +105,7 @@ public class Percolation {
 
     // // does the system percolate?
     public boolean percolates() {
-        return QU.connected(inputN * inputN, inputN * inputN + 1);
+        return qU.connected(inputN * inputN, inputN * inputN + 1);
     }
 
 
@@ -116,25 +117,25 @@ public class Percolation {
         }
         Percolation testPrec = new Percolation(n);
 
-        // testPrec.open(0, 0);
-        // System.out.printf("%b", testPrec.isOpen(0, 0));
-        // System.out.println();
-        //
-        // System.out.printf("%b", testPrec.percolates());
-        // System.out.println();
-        //
-        // testPrec.open(1, 0);
-        // System.out.printf("%b", testPrec.isOpen(1, 0));
-        // System.out.println();
-        //
-        // System.out.printf("%b", testPrec.percolates());
-        // System.out.println();
-        //
-        // testPrec.open(2, 0);
-        // System.out.printf("%b", testPrec.isOpen(2, 0));
-        // System.out.println();
-        //
-        // System.out.printf("%b", testPrec.percolates());
-        // System.out.println();
+        testPrec.open(0, 0);
+        System.out.printf("%b", testPrec.isOpen(0, 0));
+        System.out.println();
+
+        System.out.printf("%b", testPrec.percolates());
+        System.out.println();
+
+        testPrec.open(1, 0);
+        System.out.printf("%b", testPrec.isOpen(1, 0));
+        System.out.println();
+
+        System.out.printf("%b", testPrec.percolates());
+        System.out.println();
+
+        testPrec.open(2, 0);
+        System.out.printf("%b", testPrec.isOpen(2, 0));
+        System.out.println();
+
+        System.out.printf("%b", testPrec.percolates());
+        System.out.println();
     }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -2,36 +2,65 @@
  *  Name:
  *  Date:
  *  Description:
+ *
  **************************************************************************** */
-// import edu.princeton.cs.algs4.StdRandom;
-// import edu.princeton.cs.algs4.StdStats;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private int[] resultArray;
+    private int trialInput;
 
     // perform independent trials on an n-by-n grid
-    // public PercolationStats(int n, int trials) {
-    // }
-    //
-    // // sample mean of percolation threshold
-    // public double mean() {
-    // }
-    //
-    // // sample standard deviation of percolation threshold
-    // public double stddev() {
-    // }
-    //
-    // // low endpoint of 95% confidence interval
-    // public double confidenceLo() {
-    // }
-    //
-    // // high endpoint of 95% confidence interval
-    // public double confidenceHi() {
-    // }
-    //
-    // // test client (see below)
-    // public static void main(String[] args) {
-    //     /*
-    //     Also, include a main() method that takes two command-line arguments n and T, performs T independent computational experiments (discussed above) on an n-by-n grid, and prints the sample mean, sample standard deviation, and the 95% confidence interval for the percolation threshold. Use StdRandom to generate random numbers; use StdStats to compute the sample mean and sample standard deviation.
-    //     */
-    // }
+    public PercolationStats(int n, int trials) {
+        int count;
+        Percolation percolation;
+        trialInput = trials;
+        resultArray = new int[trials];
+        for (int i = 0; i < trials; i++) {
+            count = 0;
+            percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                percolation.open(StdRandom.uniform(0, n), StdRandom.uniform(0, n));
+                count++;
+            }
+            resultArray[i] = count;
+        }
+    }
 
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(resultArray);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(resultArray);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - (1.96 * stddev()) / Math.sqrt(trialInput);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + (1.96 * stddev()) / Math.sqrt(trialInput);
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        int n = 20;
+        int T = 20;
+        if (args.length == 2) {
+            n = Integer.parseInt(args[0]);
+            T = Integer.parseInt(args[1]);
+        }
+        PercolationStats percolationStats = new PercolationStats(n, T);
+        System.out.printf("mean = %f%n", percolationStats.mean());
+        System.out.printf("stddev =% f%n", percolationStats.stddev());
+        System.out.printf("95%% confidence interval = %f%n", percolationStats.confidenceLo());
+        System.out.printf("95%% confidence interval = %f%n", percolationStats.confidenceHi());
+    }
 }
